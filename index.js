6c35feaabd5f3034e306751c89cb9e48deaf7b2e
EC2 Default User <ec2-user@ip-172-31-1-24.ap-northeast-2.compute.internal>
2017-06-20 17:07:00 +0000

    closingserver

--- index.js
+++ index.js
@@ -16,135 +16,31 @@ var ioInstApp = express();
 var ioInstServer = http.Server(ioInstApp);
 var ioInst = require('socket.io')(ioInstServer, {'pingInterval': 1000, 'pingTimeout': 3000});
 
-//// socket.io service - for Monitoring client (:5700)
-var ioMonApp = express();
-var ioMonServer = http.Server(ioMonApp);
-var ioMon = require('socket.io')(ioMonServer);
-
-////shared
-var seats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-//
-// server stat protocol example...
-//
-// example stat)
-//
-// 'prog' == 'gymsession'
-// 'sched_start' == clock time
-// 'sched_stop' == clock time
-//
-var stat = {
-    'prog':'wait',
-    // 'sched_start':0,
-    // 'sched_stop':0,
-    'ctime':0
-};
-
-ioMon.on('connection', function(socket){
-    //
-    console.log('a monitoring user connected!');
-
-    // //periodically report server stat.
-    // var rollcnt = 0;
-    // var stat_reporter = setInterval(function() {
-    // 	rollcnt++;
-    // 	socket.emit('stat', {
-    // 	    'rollcnt': rollcnt,
-    // 	    'seats': seats,
-    // 	    'stat': stat
-    // 	});
-    // }, 1000);
-    
-    //announcements
-    socket.on('54321-all',        function() { ioInst.emit('54321'); });
-    socket.on('10meg-all',        function() { ioInst.emit('10meg'); });
-    socket.on('ansanintro-all',   function() { ioInst.emit('ansanintro'); });
-    socket.on('citizenintro-all', function() { ioInst.emit('citizenintro'); });
-    socket.on('clap-all',         function() { ioInst.emit('clap'); });
-    socket.on('enablespk-all',    function() { ioInst.emit('enablespk'); });
-    socket.on('enablespk-w-all',  function() { ioInst.emit('enablespk-w'); });
-    socket.on('maxvol-d-all',     function() { ioInst.emit('maxvol-d'); });
-    socket.on('maxvol-w-all',     function() { ioInst.emit('maxvol-w'); });
-    socket.on('playhelp-all',     function() { ioInst.emit('playhelp'); });
-    socket.on('spkon-all',        function() { ioInst.emit('spkon'); });
-    socket.on('spkon-slow-all',   function() { ioInst.emit('spkon-slow'); });
-    socket.on('spkon-w-all',      function() { ioInst.emit('spkon-w'); });
-    socket.on('trybutton-w-all',  function() { ioInst.emit('trybutton-w'); });
-    socket.on('webpage2-w-all',   function() { ioInst.emit('webpage2-w'); });
-    socket.on('webpage-w-all',    function() { ioInst.emit('webpage-w'); });
-
-    // //play! (sound#)
-    // socket.on('playall-start', function(msg){
-    // 	console.log('playall-start: ' + msg);
-    // 	ioInst.emit('playall-start', msg); //broadcast
-    // });
-    
-    // //stop! (sound#)
-    // socket.on('playall-stop', function(msg){
-    // 	console.log('playall-stop: ' + msg);
-    // 	ioInst.emit('playall-stop', msg); //broadcast
-    // });
-
-    //schedule --> no schedule, will fire sound immediately
-    socket.on('schedule', function(msg) {
-    	console.log('schedule: ' + msg);
-    	console.log('schedule.prog: ' + msg.prog);
-    	stat.prog = msg.prog;
-    	// stat.sched_start = msg.sched_start;
-    	// stat.sched_stop = msg.sched_stop;
-    	stat.ctime = Date.now();
-    	ioInst.emit('schedule', stat); //broadcast
-    });
-    
-    //
-    socket.on('disconnect', function(){
-    	console.log('monitoring user disconnected');
-	// clearInterval(stat_reporter);
-    });
-});
-
 ioInst.on('connection', function(socket){
 
     //
     console.log('a instrument user connected');
     
-    // //
-    // stat.ctime = Date.now();
-    // socket.emit('schedule', stat);
+    //msg. for everybody - oneshot sounds
+    socket.on('sound', function(msg) {
+	socket.broadcast.emit('sound', msg);
+	console.log('sound :' + msg);
+    });
 
-    // //
-    // var seatNo = -1;
-    // socket.on('seatsel', function(msg){
-    // 	console.log('got message seatsel : ' + msg);
-    // 	console.log('which means seat# : ' + (msg+1));
-    // 	seats[msg] = 1; // we won't care colliding selections.
-    // 	seatNo = msg; // remember for later!
-    // });
+    //msg. for everyone - notes
+    socket.on('sing-note', function(msg) {
+	socket.broadcast.emit('sing-note', msg);
+	console.log('sing-note :' + msg);
+    });
 
     //
     socket.on('disconnect', function(){
     	console.log('instrument user disconnected');
-	
-	// // clear the flag : again, we won't care colliding selections!
-	// if (seatNo != -1) {
-	//     seats[seatNo] = 0;
-	// }
     });
-
-    // //
-    // socket.on('query-schedule', function() {
-    // 	console.log('query-schedule');
-    // 	stat.ctime = Date.now();
-    // 	socket.emit('schedule', stat);
-    // });
 });
 
 ioInstServer.listen(5500, function(){
     console.log('[socket.io] listening on *:5500');
-});
-
-ioMonServer.listen(5700, function(){
-    console.log('[socket.io] listening on *:5700');
 });
 
 //// osc.js/udp service
